46c9e89b3081fa95289c727fd4e7ec1a6ae8f721
yani01 <yacine.ben010@hotmail.fr>
2017-07-13 18:06:51 +0200

    feature: Added new availability map #4401

--- daily.sh
+++ daily.sh
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+#add new fonctionalité for this file 
 cd "$(dirname "$0")"
 arg="$1"
 
